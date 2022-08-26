8d961b9d6815587e1d3adbc71eaae24e05f8039e
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-08-26 15:57:26 +0500

    123213213123

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return (<div className="App">
+      <Routes>
+        <Route path="/register" element={<h1>sadasda</h1>} />
+
+        <Route path="*" element={<h1>404 not Found</h1>} />
+      </Routes>
+    </div>);
 }
 
 export default App;
