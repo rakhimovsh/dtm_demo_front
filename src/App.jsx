23b40d39b8e20345153e6e4b1cd429cc3cd38fb9
XurshidJurayev1 <73391206+XurshidJurayev1@users.noreply.github.com>
2022-08-26 16:27:39 +0500

    back button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
+import Register from './pages/Register';
 
 function App() {
   return (<div className="App">
-      <Routes>
-        <Route path="/register" element={<h1>sadasda</h1>} />
+    <Routes>
+      <Route path="/register" element={<Register />} />
 
-        <Route path="*" element={<h1>404 not Found</h1>} />
-      </Routes>
-    </div>);
+      <Route path="*" element={<h1>404 not Found</h1>} />
+    </Routes>
+  </div>);
 }
 
 export default App;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { HiArrowNarrowLeft } from 'react-icons/hi';
+import './Register.scss';
+import Back from '../components/Back';
 
 const Register = () => {
   return (
-    <div>
-      
+    <div className="container">
+      <Back />
+      <div className="form-container">
+        <form>
+          asdadasdas
+        </form>
+      </div>
     </div>
   );
 };
